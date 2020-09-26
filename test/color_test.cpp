fbd3c1d94285b888c67b6c971497e275d0ba18b9
Chris Ohk <utilForever@gmail.com>
2020-09-26 17:38:23 +0900

    [WIP] test: Add simple test for checking color related functions

--- test/color_test.cpp
+++ test/color_test.cpp
@@ -1,4 +1,13 @@
+#include "styler.hpp"
+
+using namespace styler;
+
 int main()
 {
+    std::cout << "\n\n\n"
+              << Style::Reset << Style::Underline << Style::Bold
+              << "Control = Auto" << Style::Reset << Background::Reset
+              << Foreground::Reset << std::endl;
+
     return 0;
 }
